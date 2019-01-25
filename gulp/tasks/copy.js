c33946d386bb005ed0fab15192ba3f9c3ed7a365
Yuliya Yakutsik <juliayakutik@gmail.com>
2019-01-25 13:02:21 +0200

    update generator

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,6 +2,11 @@ import gulp         from 'gulp';
 import settings     from '../settings';
 
 gulp.task('copy', () => {
-    return gulp.src(settings.src.fonts + '/**')
+    gulp.src(settings.src.fonts + '/**')
         .pipe(gulp.dest(settings.dist.fonts));
+
+    gulp.src(settings.src.images + '/favicon/**')
+        .pipe(gulp.dest(settings.dist.images + '/favicon'));
+
+    return true;
 });
